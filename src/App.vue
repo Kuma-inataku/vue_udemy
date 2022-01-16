c7b769b2cddffa8ca26cc7ee1618f04faf7cfed0
Kuma-inataku <inataku0121@gmail,com>
2022-01-16 22:10:44 +0900

    v-modelの使用

--- src/App.vue
+++ src/App.vue
@@ -10,6 +10,16 @@
   <keep-alive>
     <component :is="currentComponent"></component>
   </keep-alive>
+  <div>
+    <h2>イベントのフォーム</h2>
+    <label for="title">タイトル</label>
+    <input
+      id="title"
+      type="text"
+      v-model="eventData.title"
+    >
+    <p>{{ eventData.title }}</p>
+  </div>
 </div>
 </template>
 
@@ -26,6 +36,9 @@ export default {
     return {
       number: 10,
       currentComponent: "Home",
+      eventData: {
+        title: "タイトル",
+      }
     }
   },
   components: {
@@ -40,9 +53,3 @@ export default {
   }
 }
 </script>
-
-<style scoped>
-  div {
-    border: 1px solid blue;
-  }
-</style>
