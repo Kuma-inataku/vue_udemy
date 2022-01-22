cb40093e740b84932230138d2b306c350546e709
Kuma-inataku <inataku0121@gmail,com>
2022-01-22 17:56:58 +0900

    add link

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,32 @@
 <template>
   <div>
+    <nav>
+      <router-link
+        to="/"
+        class="link"
+        active-class="link--active"
+        exact
+      >
+        Home
+      </router-link>
+      <router-link
+        to="/users"
+        active-class="link--active"
+        class="link"
+        exact
+      >
+        Users
+      </router-link>
+    </nav>
     <router-view></router-view>
   </div>
 </template>
+
+<style scoped>
+.link {
+  margin-right: 10px;
+}
+.link--active {
+  font-size: 20px;
+}
+</style>
