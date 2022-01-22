989416a11fc55562b79f83a8e564b51bc2a7e751
Kuma-inataku <inataku0121@gmail,com>
2022-01-23 08:52:32 +0900

    use beforEach

--- src/main.js
+++ src/main.js
@@ -4,6 +4,16 @@ import LikeNumber from './components/LikeNumber.vue';
 import router from './router';
 
 Vue.config.productionTip = false
+
+router.beforeEach((to, from, next) => {
+  console.log(to);
+  console.log(from);
+  if (to.path === "/users/1") {
+    next({ path: '/'});
+  }
+  next();
+});
+
 Vue.component("LikeNumber", LikeNumber);
 Vue.filter("upperCase", function (value) {
   return value.toUpperCase();
