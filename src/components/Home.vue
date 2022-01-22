5194ed9825a49a337dd336a0dde31f67836f4265
Kuma-inataku <inataku0121@gmail,com>
2022-01-22 16:02:18 +0900

    filter global and local register

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,3 +1,38 @@
 <template>
-  <p>home</p>
+  <div>
+  <p v-border:solid.round.shadow="{width: '3px', color: 'red'}">{{ tmpData }}</p>
+  <h2>{{ title | lowerCase }}</h2>
+  <h2>{{ subTitle | upperCase }}</h2>
+  <h2>{{ subTitle | upperCase | lowerCase }}</h2>
+  </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      tmpData: "hello",
+      title: "Welcome to Tokyo!!",
+      subTitle: "Tokyo is Great!!",
+    }
+  },
+  filters: {
+    lowerCase(value) {
+      return value.toLowerCase();
+    }
+  },
+  directives: {
+    border(el, binding) {
+      el.style.borderWidth = binding.value.width;
+      el.style.borderColor = binding.value.color;
+      el.style.borderStyle = binding.arg;
+      if (binding.modifiers.round) {
+        el.style.borderRadius = "0.5rem";
+      }
+      if (binding.modifiers.shadow) {
+        el.style.boxShadow = "0 2px 10px rgba(0, 0, 0, 1)";
+      }
+    }
+  }
+}
+</script>
