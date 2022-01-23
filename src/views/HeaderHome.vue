efaae4782fed24b14010fafd7cc53787c5ccdc3b
Kuma-inataku <inataku0121@gmail,com>
2022-01-23 21:23:12 +0900

    use mutation

--- src/views/HeaderHome.vue
+++ src/views/HeaderHome.vue
@@ -28,11 +28,11 @@
 export default {
   methods: {
     increment() {
-      this.$store.state.count++;
+      this.$store.commit('increment', 2);
     },
     decrement() {
-      this.$store.state.count--;
-    }
+      this.$store.commit('decrement', 2);
+    },
   }
 }
 </script>
