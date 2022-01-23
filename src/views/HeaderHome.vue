62d0b92fbfdcc5c547eeec9433318ec28373a42b
Kuma-inataku <inataku0121@gmail,com>
2022-01-23 09:43:46 +0900

    install vuex

--- src/views/HeaderHome.vue
+++ src/views/HeaderHome.vue
@@ -11,16 +11,31 @@
         Home
       </router-link>
       <router-link
-:to="{ name: 'users-id-profile', params: {id: 1 }, query: { lang: 'ja', page: 2 }, hash:'#next-user' } "
+        :to="{ name: 'users-id-profile', params: {id: 1 }, query: { lang: 'ja', page: 2 }, hash:'#next-user' } "
         active-class="link--active"
         class="link"
         exact
       >
         Users
       </router-link>
+      <button @click="increment">+1</button>
+      <button @click="decrement">-1</button>
     </nav>
   </div>
 </template>
+
+<script>
+export default {
+  methods: {
+    increment() {
+      this.$store.state.count++;
+    },
+    decrement() {
+      this.$store.state.count--;
+    }
+  }
+}
+</script>
 
 <style scoped>
 .link {
