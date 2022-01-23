243693669f04b632d480281e203421e2d2604bfa
Kuma-inataku <inataku0121@gmail,com>
2022-01-23 09:26:11 +0900

    use lasy load to every component

--- src/views/Users.vue
+++ src/views/Users.vue
@@ -25,24 +25,5 @@
 <script>
 export default {
   props: ["id"],
-  beforeRouteEnter(to, from, next) {
-    console.log('beforeRouteEnter');
-    next(vm => {
-      console.log(vm.id);
-    });
-  },
-  beforeRouteUpdate(to, from, next) {
-    console.log('beforeRouteUpdate');
-    next();
-  },
-  beforeRouteLeave(to, from, next) {
-    console.log('beforeRouteLeave');
-    const isLeave = window.confirm("本当に？");
-    if(isLeave){
-      next();
-    }else{
-      next(false);
-    }
-  }
 }
 </script>
