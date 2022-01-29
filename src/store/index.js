5dd29456626d404d3df593a7c36a30f376988f97
Kuma-inataku <inataku0121@gmail,com>
2022-01-29 18:36:06 +0900

    ゲッター、ミューテーション、アクションの切り分け

--- src/store/index.js
+++ src/store/index.js
@@ -1,27 +1,19 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
-import count from './modules/count'
+import count from './modules/count';
+import getters from './getters';
+import mutations from './mutations';
+import actions from './actions';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-    count: 2,
     message: "",
   },
-  getters: {
-    message: state => state.message,
-  },
-  mutations: {
-    updateMessage(state, newMessage) {
-      state.message = newMessage;
-    }
-  },
-  actions: {
-    updateMessage({ commit }, newMessage) {
-      commit("updateMessage", newMessage);
-    }
-  },
+  getters, 
+  mutations, 
+  actions,
   modules: {
     count,
   }
